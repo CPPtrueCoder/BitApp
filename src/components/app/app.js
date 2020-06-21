6282568891f5db63745ea31800e85a44e711b044
CPPtrueCoder <andierodin@gmail.com>
2020-06-21 03:52:21 +0300

    Preparing starting structure for working: Bootstrap has been connected

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+
+export default class App extends Component{
+	
+	render() {
+		return(
+			<h1 className="h1">This is Bitcoin Application</h1>
+		)
+	}
+	
+}
